dc1795be4ab6ad8f819e1055f057ef40ef23c2b9
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-10-14 15:16:35 +1100

    write TakeDamage method

--- src/models/villain.ts
+++ src/models/villain.ts
@@ -10,4 +10,10 @@ export class Villain {
         this.maxHitPoints = Math.floor(Math.random() * 10) + 1;
         this.currentHitPoints = this.maxHitPoints;
     }
+    takeDamage(damage: number): void {
+        this.currentHitPoints -= damage;
+        if (this.currentHitPoints < 0) {
+            this.currentHitPoints = 0;
+        }
+    }
 }
