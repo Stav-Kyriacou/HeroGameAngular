6bf6949422931d36d31086ecdfd5a26021553a4f
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-10-14 11:55:44 +1100

    add villain list, change styling and add ngfor for villain component

--- src/components/all-villains/all-villains.component.ts
+++ src/components/all-villains/all-villains.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Villain } from 'src/models/villain';
 
 @Component({
   selector: 'app-all-villains',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-villains.component.css']
 })
 export class AllVillainsComponent implements OnInit {
-
+  villainList: Villain[] = [new Villain(1, "Villain One"),
+                            new Villain(2, "Villain Two"),
+                            new Villain(3, "Villain Three")
+                          ];
   constructor() { }
 
   ngOnInit(): void {
