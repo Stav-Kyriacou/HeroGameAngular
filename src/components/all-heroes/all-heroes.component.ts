78221e176ce2c001a65ce7abc91f0330237c38d7
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-10-14 11:54:37 +1100

    add heroList

--- src/components/all-heroes/all-heroes.component.ts
+++ src/components/all-heroes/all-heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/models/hero';
 
 @Component({
   selector: 'app-all-heroes',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-heroes.component.css']
 })
 export class AllHeroesComponent implements OnInit {
+  heroList: Hero[] = [new Hero(1, "Hero One", 1, 2, 3),
+                      new Hero(2, "Hero Two", 4, 5, 6),
+                      new Hero(3, "Hero Three", 7, 8, 9)];
 
   constructor() { }
 
