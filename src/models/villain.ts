40a4ba57c7ccc2013a78390dd8d76a49fb6acf76
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-10-14 11:58:22 +1100

    add max and current HP values

--- src/models/villain.ts
+++ src/models/villain.ts
@@ -1,9 +1,13 @@
 export class Villain {
     id: number;
     name: string;
+    maxHitPoints: number;
+    currentHitPoints: number;
 
     constructor(id: number, name: string) {
         this.id = id;
         this.name = name;
+        this.maxHitPoints = Math.floor(Math.random() * 10) + 1;
+        this.currentHitPoints = this.maxHitPoints;
     }
 }
