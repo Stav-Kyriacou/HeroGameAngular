cfab0118a14e7c1214411bec416b953fc969bf48
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-10-21 18:45:31 +1100

    add error and onComplete methods for the get all heroes api request

--- src/components/game/game.component.ts
+++ src/components/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular
 import { Hero } from 'src/models/hero';
 import { Villain } from 'src/models/villain';
 import { HeroService } from 'src/services/hero.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-game',
@@ -9,13 +10,13 @@ import { HeroService } from 'src/services/hero.service';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  heroList: Hero[] = [new Hero(1, "Hero One", 1, 6, 3),
-                      new Hero(2, "Hero Two", 1, 6, 3),
-                      new Hero(3, "Hero Three", 1, 6, 3)];
-  // heroList: Hero[];
+  // heroList: Hero[] = [new Hero(1, "Hero One", 1, 6, 3),
+  //                     new Hero(2, "Hero Two", 1, 6, 3),
+  //                     new Hero(3, "Hero Three", 1, 6, 3)];
+  heroList: Hero[];
   villainList: Villain[] = [new Villain(1, "Villain One", 1, 10),
-                            new Villain(2, "Villain Two", 1, 10),
-                            new Villain(2, "Villain Three", 1, 10)];
+  new Villain(2, "Villain Two", 1, 10),
+  new Villain(2, "Villain Three", 1, 10)];
   selectedHero: Hero;
   selectedVillain: Villain;
   totalUsesRemaining: number = 0;
@@ -25,16 +26,18 @@ export class GameComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    // this._heroService.getAllHeroes().subscribe(heroes => this.heroList = heroes);
-    this.selectedHero = this.heroList[0];
-    this.selectedVillain = this.villainList[0];
+    this._heroService.getAllHeroes().subscribe(heroes => this.heroList = heroes,
+      error => console.log("Error: " + error),
+      () => { this.init() });
   }
-  ngAfterViewInit() {
 
+  init(): void {
+    if (this.heroList != null)
+      this.selectedHero = this.heroList[0];
+    if (this.villainList != null)
+      this.selectedVillain = this.villainList[0];
   }
-  ngAfterContentInit() {
 
-  }
   attack(): void {
     const combatLog: HTMLElement = <HTMLElement>document.getElementById('combatLog');
     this.checkHpRemaining();
@@ -47,6 +50,7 @@ export class GameComponent implements OnInit {
     }
     this.checkWin();
   }
+
   selectChange(): void {
     const heroSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('hero-select');
     const villainSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('villain-select');
@@ -54,6 +58,7 @@ export class GameComponent implements OnInit {
     this.selectedHero = this.heroList[heroSelect.selectedIndex];
     this.selectedVillain = this.villainList[villainSelect.selectedIndex];
   }
+
   checkWin(): void {
     //counts heroes running out of action on the same turn that all the villains die as a win
     if ((this.totalUsesRemaining > 0 && this.totalHpRemaining <= 0) || (this.totalUsesRemaining == 0 && this.totalHpRemaining <= 0)) {
@@ -63,6 +68,7 @@ export class GameComponent implements OnInit {
       console.log("Villains win. Heroes ran out of turns");
     }
   }
+
   checkHpRemaining(): void {
     this.totalHpRemaining = 0;
 
@@ -70,6 +76,7 @@ export class GameComponent implements OnInit {
       this.totalHpRemaining += v.currentHitPoints;
     }
   }
+  
   checkUsesRemaining(): void {
     this.totalUsesRemaining = 0;
 
