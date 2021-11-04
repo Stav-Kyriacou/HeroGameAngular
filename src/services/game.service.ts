bc836054fb3273190c9fffd54daf3390c53f2bb9
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-11-04 20:21:29 +1100

    create get and post methods

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Game } from 'src/models/game';
+import { BaseService } from './base.service';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class GameService {
+export class GameService extends BaseService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    super();
+  }
+  getAllGames(): Observable<Game[]> {
+    return this._http.get<Game[]>(this.baseUrl + "Game");
+  }
+  addNewGame(newGame: Game): Observable<Game> {
+    return this._http.post<Game>(this.baseUrl + "Game", newGame);
+  }
 }
